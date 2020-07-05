2a53e8b641785347bf659f26494cfc21573d211b
Dominik Baran <dominik.baran1992@gmail.com>
2020-07-05 19:26:53 +0200

    Add rates

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import main
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def calculator():
     calculator = main.Currency_Calculator()
@@ -16,14 +15,18 @@ def calculator():
         return render_template('form.html', items=items)
 
     elif request.method == 'POST':
-        data = request.form
-        currency_code = data.get('currency')
-        quantity = data.get('quantity')
-
-        for element in calculator.rates:
-            if currency_code == element['code']:
-                code=element['code']
-                value = float(quantity)*element['bid']
-
-        return render_template('form.html', value=value, code=code)
-
+        return redirect('/rate')
+        
+@app.route('/rate', methods=['POST'])
+def rates():
+    calculator = main.Currency_Calculator()
+    calculator.get_rates()
+    data = request.form
+    currency_code = data.get('currency')
+    quantity = data.get('quantity')
+    for element in calculator.rates:
+        if currency_code == element['code']:
+            code=element['code']
+            value = round(float(quantity)*element['bid'],2)
+    return render_template('form.html', value=value, code=code, quantity=quantity)
+    
