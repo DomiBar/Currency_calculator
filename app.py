3ecf20573562d4fa237da1578392fa3e25a9797b
Dominik Baran <dominik.baran1992@gmail.com>
2020-07-05 18:54:02 +0200

    Add web app

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template, request, redirect()
+from flask import Flask, render_template, request, redirect
 import main
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/calculator', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def calculator():
-    pass
+    calculator = main.Currency_Calculator()
+    calculator.get_rates()
+    if request.method == 'GET':
+        items = []
+        for element in calculator.rates:
+            items.append(element['code'])
+
+        return render_template('form.html', items=items)
+
+    elif request.method == 'POST':
+        data = request.form
+        currency_code = data.get('currency')
+        quantity = data.get('quantity')
+
+        for element in calculator.rates:
+            if currency_code == element['code']:
+                code=element['code']
+                value = float(quantity)*element['bid']
+
+        return render_template('form.html', value=value, code=code)
+
--- main.py
+++ main.py
@@ -2,24 +2,29 @@ import json
 import requests
 import csv
 
-def export_to_csv(rates):
-    file_csv='rates.csv'
-    try:
-        with open(file_csv,'w', newline='') as csvfile:
-            fieldnames=['currency','code','bid','ask']
-            writer=csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
-            writer.writerow({'currency':'Waluta','code':'Kod','bid':'Cena kupna','ask':'Cena sprzedaży'})
-            writer.writerows(rates)    
-    except:
-        print(f"!!!!!!Nie udało się otworzyć pliku {file_csv} . Dane nie zostały zapisane.!!!!!!\nJeżeli plik jest otwarty w innym programie, zamknij go przed zapisem.")
 
-rates=[]
+class Currency_Calculator:
+    def __init__(self):
+        self.rates=[]
 
-response=requests.get('http://api.nbp.pl/api/exchangerates/tables/C?format=json')
-data=response.json()
+    def __str__(self):
+        return str(self.rates)
+    
+    def get_rates(self):
+        response=requests.get('http://api.nbp.pl/api/exchangerates/tables/C?format=json')
+        data=response.json()
 
-for key in data[0]['rates']:
-    rates.append(key)
+        for key in data[0]['rates']:
+            self.rates.append(key)
 
-export_to_csv(rates)
+    def export_to_csv(self):
+        file_csv='rates.csv'
+        try:
+            with open(file_csv,'w', newline='') as csvfile:
+                fieldnames=['currency','code','bid','ask']
+                writer=csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+                writer.writerow({'currency':'Waluta','code':'Kod','bid':'Cena kupna','ask':'Cena sprzedaży'})
+                writer.writerows(self.rates)    
+        except:
+            print(f"!!!!!!Nie udało się otworzyć pliku {file_csv} . Dane nie zostały zapisane.!!!!!!\nJeżeli plik jest otwarty w innym programie, zamknij go przed zapisem.")
 
